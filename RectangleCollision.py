16c95bd6811b4813352fc32bf08f4c6c7baa72e7
unknown <jacob_vosburgh@yahoo.com>
2019-04-24 23:50:14 -0400

    rectangles now fully operational

--- RectangleCollision.py
+++ RectangleCollision.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 22:41:18 2019
+
+@author: Vosburgh
+"""
+
 import numpy
 import os
 import pycuda.autoinit
@@ -9,14 +16,75 @@ from pycuda.compiler import SourceModule
 import pycuda.gpuarray as gpuarray
 from Shapes import Rectangle
 
-#TODO
-def generateRandomRectangles():
-    pass
+#if on windows, try to find cl.exe
+if os.name=='nt':
+    if (os.system("cl.exe")):
+        os.environ['PATH'] += ';'+r"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64"
+        if (os.system("cl.exe")):
+            raise RuntimeError("cl.exe still not found, path probably incorrect")
 
-#TODO
+def generateRandomRectangles(numRectangles, x_range = range(1,400), y_range = range(1,400), size_range = range(5,50)):
+    random.seed()
+    x1 = [random.choice(x_range) for i in range(numRectangles)]
+    y1 = [random.choice(y_range) for i in range(numRectangles)]
+    x2 = numpy.zeros(numRectangles)
+    y2 = numpy.zeros(numRectangles)
+    for i in range(numRectangles):
+        x2[i] = x1[i] + random.choice(size_range)
+        print(x1[i] , ", " , x2[i])
+    for i in range(numRectangles):
+        y2[i] = y1[i] + random.choice(size_range) 
+        print(y1[i] , ", " , y2[i])
+    rectangles = [Rectangle(x1[i],y1[i],x2[i],y2[i]) for i in range(numRectangles)]
+    return rectangles   
+
 def detectCollisionGPU(robot, obstacles):
-    pass
+    print("compiling kernel")
+    mod = SourceModule("""
+    __global__ void check_collisions(
+        float x1_robot, float y1_robot, float x2_robot, float y2_robot,
+        float *x1_obs, float *y1_obs, float *x2_obs, float *y2_obs,
+        bool *collisions, int *indexes)
+    {
+        int obstacleId = threadIdx.x;
+        bool xcol = ((x1_obs[obstacleId] <= x1_robot && x1_robot <= x2_obs[obstacleId])|| (x1_obs[obstacleId] <= x2_robot && x2_robot <= x2_obs[obstacleId])) || ( x1_robot <= x1_obs[obstacleId] && x2_robot >= x2_obs[obstacleId]);
+        bool ycol = ((y1_obs[obstacleId] <= y1_robot && y1_robot <= y2_obs[obstacleId])|| (y1_obs[obstacleId] <= y2_robot && y2_robot <= y2_obs[obstacleId])) || ( y1_robot <= y1_obs[obstacleId] && y2_robot >= y2_obs[obstacleId]);
+        collisions[obstacleId] = (xcol && ycol);
+    }
+    """)
+    print("compiled kernel")
 
-#TODO
+    
+    check_collisions = mod.get_function("check_collisions")
+    
+    
+    #constants that will be passed directly to kernel
+    x1_robot = numpy.float32(robot.x1)
+    y1_robot = numpy.float32(robot.y1)
+    x2_robot = numpy.float32(robot.x2)
+    y2_robot = numpy.float32(robot.y2)
+    #allocating arrays on the gpu for obstacle coordinates and radii
+    x1_obs_gpu = gpuarray.to_gpu(numpy.asarray([rectangle.x1 for rectangle in obstacles]).astype(numpy.float32))#nVidia only supports single precision)
+    y1_obs_gpu = gpuarray.to_gpu(numpy.asarray([rectangle.y1 for rectangle in obstacles]).astype(numpy.float32))
+    x2_obs_gpu = gpuarray.to_gpu(numpy.asarray([rectangle.x2 for rectangle in obstacles]).astype(numpy.float32))
+    y2_obs_gpu = gpuarray.to_gpu(numpy.asarray([rectangle.y2 for rectangle in obstacles]).astype(numpy.float32))
+
+
+    collisions = numpy.zeros(len(obstacles), dtype=bool)
+    print(collisions)
+    
+    gpuStart = time.time()
+    check_collisions(
+            x1_robot, y1_robot, x2_robot, y2_robot,
+            x1_obs_gpu, y1_obs_gpu, x2_obs_gpu, y2_obs_gpu,
+            drv.InOut(collisions),
+            block=(len(obstacles),1,1), grid=(1,1))
+    print(collisions)
+
+    print("gpu time taken = "+str(time.time()-gpuStart))
+
+    return collisions
+
 def detectCollisionCPU(robot, obstacles):
     pass
+
